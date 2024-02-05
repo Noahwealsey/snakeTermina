087d37017fa30678bcff2f715a9dd235f9d71123
Elijah Frost <harshgaur563@gmail.com>
2024-02-06 01:22:55 +0530

    v1.0

--- snake.c
+++ snake.c
@@ -1,52 +1,96 @@
-/* File Path: basics/hello_world.c */
-#include <ncurses.h>
-#include <unistd.h>
-#include <string.h>
 #include <curses.h>
 #include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
 
-int main(int argc, char *argv[])
-{       
+typedef struct 
+{
+    int x;
+    int y;
+} vector2d;
 
-    int exitCode;
-    FILE *fp;
-    int goto_prev = FALSE , y, x;
+int hash() {
+    return rand() % 5;
+}
 
-    char msg[] = "Hello World !!!";
-    int i,j;
-    char str[88];
+vector2d output(vector2d input) {
+    input.x = hash();
+    input.y = hash();
+    return input;
+}
 
-    initscr();                      /* Start curses mode              */
-    if(has_colors() == FALSE){
-        endwin();
-        printf("you terminal sucks");
-        exit(1);
-    }
-    raw();                          //diable line buffering
-    keypad(stdscr, TRUE);
+int main() {
+    WINDOW *win = initscr();
+    keypad(win, true);
+    nodelay(win, true);
+    cbreak();
     noecho();
+    curs_set(0);  // Hide the cursor
+  
+    int ch;
+    int score = 0;
 
-    init_pair(1, COLOR_RED, COLOR_BLACK);
-    printw("Kill me");
-    mvchgat(0, 0, -1, A_BLINK, 1, NULL);
+    vector2d food = {2, 4};
+    vector2d dir = {0, 0};
+    vector2d pos = {1, 1};
+    vector2d tail[100];
 
-    getmaxyx(stdscr, i, j);
-    
-    attron(COLOR_PAIR(1));
-    mvprintw(i/2., (j - strlen(msg))/2., "%s", msg);
-    mvprintw(i - 2., 0., "the no.rows and columns in this windowa are %d, %d\n",i, j);
-    attroff(COLOR_PAIR(1));
+    int len = 1;
 
-    getstr(str);
-    mvprintw(23, 0, "You entered %s\n", str);
+    while (true) {
+        int pressed = wgetch(win);
+        switch (pressed) {
+            case KEY_RIGHT:
+                dir.x = 1;
+                dir.y = 0;
+                break;
+            case KEY_LEFT:
+                dir.x = -1;
+                dir.y = 0;
+                break;
+            case KEY_UP:
+                dir.y = -1;
+                dir.x = 0;
+                break;
+            case KEY_DOWN:
+                dir.y = 1;
+                dir.x = 0;
+                break;
+        }
 
-    printw("press any key to exit \n");
+        pos.x += dir.x;
+        pos.y += dir.y;
 
-    exitCode = getch();
+        erase();
 
-    refresh();                      /* Print it on to the real screen */ 
+        // Draw the tail if the snake has a length greater than 1
+        if (len > 1) {
+            // Draw the tail of the snake (optional)
+            for (int i = 0; i < len; i++)
+            {
+                int tailX = pos.x - dir.x;
+                int tailY = pos.y - dir.y;
+                mvaddch(tailY, tailX, '*');    
+            }
+            
+            
+        }
 
-    endwin();                       /* End curses mode                */
+        mvaddch(pos.y, pos.x, '*');
+        mvaddch(food.y, food.x, '0');
 
+        if (pos.x == food.x && pos.y == food.y) {
+            score++;
+            food = output(food);
+            len++;  // Increase the length of the snake
+            curs_set(0);  // Hide the cursor each time food appears
+        }
+
+        mvprintw(0, 0, "Score: %d", score);
+        refresh();
+        usleep(100000);
+    }
+
+    endwin();
     return 0;
 }
